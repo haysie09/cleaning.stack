603e68977d2876de4ecb45de84caf3a8b84f4c1b
haysie09 <heyitshaysie@gmail.com>
2025-08-21 23:58:44 +1200

    new

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Main Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
